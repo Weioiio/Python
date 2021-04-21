a977250fa7c246ab95f417770ed4abbf731191f1
Weioiio <hihihi0800@gmail.com>
2021-04-21 18:16:13 +0800

    mkdir folder

---
+++ Python_SHA/sha2/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 05:12:44 2019
+
+@author: hihih
+"""
+
+from sha2 import sha224
+from sha2 import sha256
+from sha2 import sha384
+from sha2 import sha512
